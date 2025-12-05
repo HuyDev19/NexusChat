9363957cbb49b3de3b62e8e20c5832ff4d3aaadb
VinhPhus <doanphanvinhphu91@gmail.com>
2025-12-05 16:27:35 +0700

    build phần đăng kí

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import SignUpPage from "./pages/SignUpPage";
+import SignInPage from "./pages/SignInPage";
+import ChatAppPage from "./pages/ChatAppPage";
+import { Toaster } from "sonner";
+
+function App() {
+  return (
+    <>
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          {/* public routes*/}
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+
+          {/* protected routes*/}
+          <Route path="/signup" element={<ChatAppPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
